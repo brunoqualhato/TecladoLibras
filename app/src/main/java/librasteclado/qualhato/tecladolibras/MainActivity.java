23fe5bac176f7666c4c6736d011f1ccaaec190af
brunoqualhato <sadraqui7894>
2018-10-29 14:06:56 -0300

    teste de codificacao

---
+++ app/src/main/java/librasteclado/qualhato/tecladolibras/MainActivity.java
@@ -0,0 +1,35 @@
+package librasteclado.qualhato.tecladolibras;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import java.lang.reflect.Field;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Typeface oTypeface= ResourcesCompat.getFont(this, R.font.libras);
+        subscreverFonte("DEFAULT",oTypeface);
+
+
+    }
+    protected static void subscreverFonte(String staticTypefaceFieldName,
+                                      final Typeface newTypeface) {
+        try {
+            final Field staticField = Typeface.class
+                    .getDeclaredField(staticTypefaceFieldName);
+            staticField.setAccessible(true);
+            staticField.set(null, newTypeface);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/librasteclado/qualhato/tecladolibras/MetodoEntradaTeclado.java
@@ -0,0 +1,87 @@
+package librasteclado.qualhato.tecladolibras;
+
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+
+public class MetodoEntradaTeclado extends InputMethodService
+        implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView tecladoVew;
+    private Keyboard teclado;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        tecladoVew = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        teclado = new Keyboard(this, R.xml.number_pad);
+        tecladoVew.setKeyboard(teclado);
+        tecladoVew.setOnKeyboardActionListener(this);
+        return tecladoVew;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                tecladoVew.setShifted(caps);
+                tecladoVew.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
